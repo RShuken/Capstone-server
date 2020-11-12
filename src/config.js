5a284d79c635a2be9dc356b5b6c9ca2af7bb0ce3
rshuken <ryanshuken@gmail.com>
2020-11-11 17:10:39 -0700

    deploying to heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 /* eslint-disable strict */
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/noteful_test'
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/noteful_test",
 };
